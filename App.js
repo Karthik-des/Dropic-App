4cdb94581de88a2002c262e5fb0117ba92072510
surya <suryaffa648@gmail.com>
2025-07-23 14:50:22 +0530

    inital works

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
-import styles from './AppCss';
 import { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import LoginScreen from './src/screens/LoginScreen/LoginScreen'
+import styles from './AppCss';
 import car from './assets/Car.webm';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [startingValue, setStartingValue] = useState(true);
@@ -14,9 +23,9 @@ export default function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  return (
-    <View style={{ flex: 1 }}>
-      {startingValue ? (
+  if (startingValue) {
+    return (
+      <View style={{ flex: 1 }}>
         <View style={styles.container}>
           <View style={styles.home}>
             <Image
@@ -26,11 +35,18 @@ export default function App() {
             <StatusBar style="auto" />
           </View>
         </View>
-      ) : (
-        <View style={styles.container}>
-          <Text style={styles.text}>Welcome to Bla Bla</Text>
-        </View>
-      )}
-    </View>
+      </View>
+    );
+  }
+
+  // After splash, show navigation
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
